e44e3812f9a3cd4df1733fbab511ca5617103ff5
彦方 <ke.like@alibaba-inc.com>
2019-04-14 11:24:06 +0800

    Support customize timespane for report

---
+++ src/main/java/com/aliyun/openservices/log/common/TimeSpan.java
@@ -0,0 +1,63 @@
+package com.aliyun.openservices.log.common;
+
+import com.aliyun.openservices.log.util.JsonUtils;
+import net.sf.json.JSONObject;
+
+import java.io.Serializable;
+
+public class TimeSpan implements Serializable {
+    private TimeSpanType type;
+    private String start;
+    private String end;
+
+    public TimeSpanType getType() {
+        return type;
+    }
+
+    public void setType(TimeSpanType type) {
+        this.type = type;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TimeSpan period = (TimeSpan) o;
+
+        if (getType() != period.getType()) return false;
+        if (getStart() != null ? !getStart().equals(period.getStart()) : period.getStart() != null) return false;
+        return getEnd() != null ? getEnd().equals(period.getEnd()) : period.getEnd() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getType() != null ? getType().hashCode() : 0;
+        result = 31 * result + (getStart() != null ? getStart().hashCode() : 0);
+        result = 31 * result + (getEnd() != null ? getEnd().hashCode() : 0);
+        return result;
+    }
+
+    public void deserialize(JSONObject timeSpan) {
+        type = TimeSpanType.fromString(timeSpan.getString("type"));
+        start = JsonUtils.readOptionalString(timeSpan, "start");
+        end = JsonUtils.readOptionalString(timeSpan, "end");
+    }
+}
