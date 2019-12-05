95a3a72b79b81d37a502a41069b213acb668f143
ke.like <ke.like@alibaba-inc.com>
2019-12-05 16:04:34 +0800

    fix key case

--- src/main/java/com/aliyun/openservices/log/common/AliyunMaxComputeSource.java
+++ src/main/java/com/aliyun/openservices/log/common/AliyunMaxComputeSource.java
@@ -4,113 +4,113 @@ import com.alibaba.fastjson.JSONObject;
 import com.aliyun.openservices.log.util.JsonUtils;
 
 public class AliyunMaxComputeSource extends DataSource {
-	private String accessKeyId;
-	private String accessKeySecret;
-	private String endpoint;
-	private String tunnelEndpoint;
-	private String project;
-	private String table;
-	private String partitionSpec;
-	private String timeField;
-	private String timeFormat;
-	private String timeZone;
+    private String accessKeyID;
+    private String accessKeySecret;
+    private String endpoint;
+    private String tunnelEndpoint;
+    private String project;
+    private String table;
+    private String partitionSpec;
+    private String timeField;
+    private String timeFormat;
+    private String timeZone;
 
-	public AliyunMaxComputeSource() {
-		super(DataSourceType.ALIYUN_MAX_COMPUTE);
-	}
+    public AliyunMaxComputeSource() {
+        super(DataSourceType.ALIYUN_MAX_COMPUTE);
+    }
 
-	public String getAccessKeyId() {
-		return accessKeyId;
-	}
+    public String getAccessKeyId() {
+        return accessKeyID;
+    }
 
-	public void setAccessKeyId(String accessKeyId) {
-		this.accessKeyId = accessKeyId;
-	}
+    public void setAccessKeyId(String accessKeyID) {
+        this.accessKeyID = accessKeyID;
+    }
 
-	public String getAccessKeySecret() {
-		return accessKeySecret;
-	}
+    public String getAccessKeySecret() {
+        return accessKeySecret;
+    }
 
-	public void setAccessKeySecret(String accessKeySecret) {
-		this.accessKeySecret = accessKeySecret;
-	}
+    public void setAccessKeySecret(String accessKeySecret) {
+        this.accessKeySecret = accessKeySecret;
+    }
 
-	public String getEndpoint() {
-		return endpoint;
-	}
+    public String getEndpoint() {
+        return endpoint;
+    }
 
-	public void setEndpoint(String endpoint) {
-		this.endpoint = endpoint;
-	}
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
 
-	public String getTunnelEndpoint() {
-		return tunnelEndpoint;
-	}
+    public String getTunnelEndpoint() {
+        return tunnelEndpoint;
+    }
 
-	public void setTunnelEndpoint(String tunnelEndpoint) {
-		this.tunnelEndpoint = tunnelEndpoint;
-	}
+    public void setTunnelEndpoint(String tunnelEndpoint) {
+        this.tunnelEndpoint = tunnelEndpoint;
+    }
 
-	public String getProject() {
-		return project;
-	}
+    public String getProject() {
+        return project;
+    }
 
-	public void setProject(String project) {
-		this.project = project;
-	}
+    public void setProject(String project) {
+        this.project = project;
+    }
 
-	public String getTable() {
-		return table;
-	}
+    public String getTable() {
+        return table;
+    }
 
-	public void setTable(String table) {
-		this.table = table;
-	}
+    public void setTable(String table) {
+        this.table = table;
+    }
 
-	public String getPartitionSpec() {
-		return partitionSpec;
-	}
+    public String getPartitionSpec() {
+        return partitionSpec;
+    }
 
-	public void setPartitionSpec(String partitionSpec) {
-		this.partitionSpec = partitionSpec;
-	}
+    public void setPartitionSpec(String partitionSpec) {
+        this.partitionSpec = partitionSpec;
+    }
 
-	public String getTimeField() {
-		return timeField;
-	}
+    public String getTimeField() {
+        return timeField;
+    }
 
-	public void setTimeField(String timeField) {
-		this.timeField = timeField;
-	}
+    public void setTimeField(String timeField) {
+        this.timeField = timeField;
+    }
 
-	public String getTimeFormat() {
-		return timeFormat;
-	}
+    public String getTimeFormat() {
+        return timeFormat;
+    }
 
-	public void setTimeFormat(String timeFormat) {
-		this.timeFormat = timeFormat;
-	}
+    public void setTimeFormat(String timeFormat) {
+        this.timeFormat = timeFormat;
+    }
 
-	public String getTimeZone() {
-		return timeZone;
-	}
+    public String getTimeZone() {
+        return timeZone;
+    }
 
-	public void setTimeZone(String timeZone) {
-		this.timeZone = timeZone;
-	}
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
 
-	@Override
-	public void deserialize(JSONObject jsonObject) {
-		super.deserialize(jsonObject);
-		accessKeyId = JsonUtils.readOptionalString(jsonObject, "accessKeyId");
-		accessKeySecret = JsonUtils.readOptionalString(jsonObject, "accessKeySecret");
-		endpoint = JsonUtils.readOptionalString(jsonObject, "endpoint");
-		tunnelEndpoint = JsonUtils.readOptionalString(jsonObject, "tunnelEndpoint");
-		project = JsonUtils.readOptionalString(jsonObject, "project");
-		table = JsonUtils.readOptionalString(jsonObject, "table");
-		partitionSpec = JsonUtils.readOptionalString(jsonObject, "partitionSpec");
-		timeField = JsonUtils.readOptionalString(jsonObject, "timeField");
-		timeFormat = JsonUtils.readOptionalString(jsonObject, "timeFormat");
-		timeZone = JsonUtils.readOptionalString(jsonObject, "timeZone");
-	}
+    @Override
+    public void deserialize(JSONObject jsonObject) {
+        super.deserialize(jsonObject);
+        accessKeyID = JsonUtils.readOptionalString(jsonObject, "accessKeyID");
+        accessKeySecret = JsonUtils.readOptionalString(jsonObject, "accessKeySecret");
+        endpoint = JsonUtils.readOptionalString(jsonObject, "endpoint");
+        tunnelEndpoint = JsonUtils.readOptionalString(jsonObject, "tunnelEndpoint");
+        project = JsonUtils.readOptionalString(jsonObject, "project");
+        table = JsonUtils.readOptionalString(jsonObject, "table");
+        partitionSpec = JsonUtils.readOptionalString(jsonObject, "partitionSpec");
+        timeField = JsonUtils.readOptionalString(jsonObject, "timeField");
+        timeFormat = JsonUtils.readOptionalString(jsonObject, "timeFormat");
+        timeZone = JsonUtils.readOptionalString(jsonObject, "timeZone");
+    }
 }
