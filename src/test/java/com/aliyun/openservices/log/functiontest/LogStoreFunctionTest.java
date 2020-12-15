52772cfe4acc9940523233c4105d992057277bf6
ke.like <ke.like@alibaba-inc.com>
2020-12-15 14:18:25 +0800

    fix update logstore test

--- src/test/java/com/aliyun/openservices/log/functiontest/LogStoreFunctionTest.java
+++ src/test/java/com/aliyun/openservices/log/functiontest/LogStoreFunctionTest.java
@@ -81,35 +81,41 @@ public class LogStoreFunctionTest extends FunctionTest {
 
     @Test
     public void testUpdateLogStore() throws Exception {
+        // todo fix this cache not deleted yet
+        waitForSeconds(60);
+
+        String logstoreName = "logstore-for-testing1";
+
         LogStore logStore = new LogStore();
-        logStore.SetLogStoreName("logstore-for-testing1");
+        logStore.SetLogStoreName(logstoreName);
         logStore.SetShardCount(2);
         logStore.SetTtl(7);
         logStore.setAppendMeta(false);
         client.CreateLogStore(TEST_PROJECT, logStore);
 
-        GetLogStoreResponse response = client.GetLogStore(TEST_PROJECT, "logstore-for-testing1");
+        GetLogStoreResponse response = client.GetLogStore(TEST_PROJECT, logstoreName);
         LogStore logStore1 = response.GetLogStore();
-        assertTrue(logStore1.isAppendMeta());
+        assertEquals(logStore1.isAppendMeta(), logStore.isAppendMeta());
         assertEquals(7, logStore1.GetTtl());
         assertEquals(2, logStore1.GetShardCount());
-        assertEquals("logstore-for-testing3", logStore1.GetLogStoreName());
+        assertEquals(logstoreName, logStore1.GetLogStoreName());
 
         logStore = new LogStore();
-        logStore.SetLogStoreName("logstore-for-testing3");
+        logStore.SetLogStoreName(logstoreName);
         logStore.SetShardCount(3);
         logStore.SetTtl(30);
         logStore.setAppendMeta(true);
         client.UpdateLogStore(TEST_PROJECT, logStore);
 
-        response = client.GetLogStore(TEST_PROJECT, "logstore-for-testing3");
+        response = client.GetLogStore(TEST_PROJECT, logstoreName);
         LogStore logStore2 = response.GetLogStore();
-        assertFalse(logStore2.isAppendMeta());
+        assertEquals(logStore.isAppendMeta(), logStore2.isAppendMeta());
         assertEquals(30, logStore2.GetTtl());
-        assertEquals(3, logStore2.GetShardCount());
-        assertEquals("logstore-for-testing3", logStore2.GetLogStoreName());
+        // shard count cannot be changed via update logstore API
+        assertEquals(2, logStore2.GetShardCount());
+        assertEquals(logstoreName, logStore2.GetLogStoreName());
 
-        client.DeleteLogStore(TEST_PROJECT, "logstore-for-testing3");
+        client.DeleteLogStore(TEST_PROJECT, logstoreName);
     }
 
     @After
