0e5018f34d2f9007741bb348a7db20af9b454ceb
ke.like <ke.like@alibaba-inc.com>
2021-05-12 14:35:39 +0800

    Update version

--- src/test/java/com/aliyun/openservices/log/functiontest/ListShardFunctionTest.java
+++ src/test/java/com/aliyun/openservices/log/functiontest/ListShardFunctionTest.java
@@ -4,8 +4,6 @@ import com.aliyun.openservices.log.common.LogStore;
 import com.aliyun.openservices.log.common.Shard;
 import com.aliyun.openservices.log.exception.LogException;
 import com.aliyun.openservices.log.response.ListShardResponse;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -15,33 +13,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-public class ListShardFunctionTest extends FunctionTest {
-
-    private final String project = "test-project-list-shard-"+getNowTimestamp();
-
-    @Before
-    public void setUp() {
-        safeCreateProject(project, "");
-    }
-
-    @After
-    public void tearDown() {
-        safeDeleteProject(project);
-    }
+public class ListShardFunctionTest extends MetaAPIBaseFunctionTest {
 
     @Test
     public void testListShard() throws LogException {
         String logstore1 = "logstore1";
         try {
-            client.ListShard(project, logstore1);
+            client.ListShard(TEST_PROJECT, logstore1);
             fail();
         } catch (LogException ex) {
             assertEquals("LogStoreNotExist", ex.GetErrorCode());
             assertEquals("logstore logstore1 does not exist", ex.GetErrorMessage());
         }
         LogStore logStore = new LogStore(logstore1, 1, 3);
-        createOrUpdateLogStore(project, logStore);
-        ListShardResponse response = client.ListShard(project, logstore1);
+        createOrUpdateLogStore(TEST_PROJECT, logStore);
+        ListShardResponse response = client.ListShard(TEST_PROJECT, logstore1);
         List<Shard> shards = response.GetShards();
         assertEquals(3, shards.size());
         List<Integer> ids = new ArrayList<Integer>();
