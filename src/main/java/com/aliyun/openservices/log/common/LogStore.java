8a815e0e2f1eb069f7f60988461fa59955ded27b
载云 <guohonggang.ghg@alibaba-inc.com>
2022-04-26 14:59:35 +0800

    getLogstore support productType

--- src/main/java/com/aliyun/openservices/log/common/LogStore.java
+++ src/main/java/com/aliyun/openservices/log/common/LogStore.java
@@ -236,7 +236,6 @@ public class LogStore implements Serializable {
         logStoreDict.put("resourceQuota", resourceQuota);
         logStoreDict.put("archiveSeconds", archiveSeconds);
         logStoreDict.put("telemetryType", telemetryType);
-        logStoreDict.put("productType", productType);
         if(hotTTL > 0) {
             logStoreDict.put("hot_ttl",hotTTL);
         }
